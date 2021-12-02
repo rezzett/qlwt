904ed495b3a543311b9ca042ce3148f32ce712b7
rezzett <rezzett@mail.ru>
2021-12-02 12:58:39 +0600

    refactor storage start

--- Storage.h
+++ Storage.h
@@ -10,98 +10,116 @@
 struct WordPair {
     QString word;
     QString translated;
-    QString lesson;
-    WordPair(const QString& w = "", const QString& t = "", const QString& lsn = ""):
-        word(w), translated(t), lesson(lsn) {}
+    WordPair(const QString& w = "", const QString& t = ""):
+        word(w), translated(t){}
 };
 
 inline QDataStream& operator << (QDataStream& out, const WordPair& wp) {
-    out << wp.word << wp.translated << wp.lesson;
+    out << wp.word << wp.translated;
     return out;
 }
 
 inline QDataStream& operator >> (QDataStream& in,  WordPair& wp) {
-    in >> wp.word >> wp.translated >> wp.lesson;
+    in >> wp.word >> wp.translated;
     return in;
 }
 
 class Storage {
-    QVector<WordPair> records;
-    QVector<WordPair> currentLesson;
+    QVector<WordPair> words;
+    QVector<WordPair> trainingWords;
     QVector<QString> lessons;
 public:
-    void load() {
-        QFile readFile("data.txt");
+    // save and load  =====================================
+    void loadWords(const QString& lessonName) {
+        QFile readFile(lessonName +".txt");
         readFile.open(QFile::ReadOnly);
         QDataStream rf(&readFile);
         while(!rf.atEnd()) {
             WordPair tmp;
             rf >> tmp;
-            records.push_back(tmp);
+            words.push_back(tmp);
         }
         readFile.close();
     }
 
-    void save() {
-        QFile writeFile("data.txt");
+    void saveWords(const QString& lessonName) {
+        QFile writeFile(lessonName + ".txt");
         writeFile.open(QFile::ReadWrite);
         QDataStream wf(&writeFile);
-        for(auto& record: records) {
-            wf << record;
+        for(auto& word: words) {
+            wf << word;
         }
         writeFile.close();
     }
 
-    void loadCurrentLesson(const QString& lessonName) {
-        currentLesson.clear();
-        std::copy_if(records.begin(), records.end(), std::back_inserter(currentLesson), [&lessonName](const WordPair& item) {
-            return item.lesson == lessonName;
-        });
-    }
-
-    const QVector<WordPair>& getCurrentLesson() const {
-        return currentLesson;
-    }
-
-    QVector<WordPair> getCurrentLesson() {
-        return currentLesson;
-    }
-
-    const QVector<QString>& getLessons() const {
-        return lessons;
-    }
-
-    void addLessonRecord(const WordPair& wp) {
-        currentLesson.append(wp);
-        records.append(wp);
-    }
-
-    void deletelLessonRecord(const QString& toRemove) {
-        currentLesson.erase( std::remove_if(currentLesson.begin(), currentLesson.end(), [&toRemove]( const WordPair& wp) {
-             return wp.word == toRemove;
-        }), currentLesson.end());
-        records.erase( std::remove_if(records.begin(), records.end(), [&toRemove]( const WordPair& wp) {
-             return wp.word == toRemove;
-        }), records.end());
-    }
-
     void loadLessons() {
-        for(auto& record: records) {
-           if(lessons.contains(record.lesson)) continue;
-           lessons.append(record.lesson);
+        QFile readFile("lessons.txt");
+        readFile.open(QFile::ReadOnly);
+        QDataStream rf(&readFile);
+        while(!rf.atEnd()) {
+            QString tmp;
+            rf >> tmp;
+            lessons.push_back(tmp);
         }
+        readFile.close();
     }
 
-    void removeLesson(const QString& lesssonName) {
-        lessons.erase(std::remove_if(lessons.begin(), lessons.end(), [&lesssonName](const QString& name) {
-            return lesssonName == name;
-        }), lessons.end());
-
-        records.erase(std::remove_if(records.begin(), records.end(), [&lesssonName](const WordPair& item) {
-            return item.lesson == lesssonName;
-        }), records.end());
-        currentLesson.clear();
+    // lessons interface =====================================
+    void saveLessons() {
+        QFile writeFile("lessons.txt");
+        writeFile.open(QFile::ReadWrite);
+        QDataStream wf(&writeFile);
+        for(auto& lesson: lessons) {
+            wf << lesson;
+        }
+        writeFile.close();
     }
+
+    const QVector<Qstring>& getLessons() const { return lessons;}
+
+    void addLesson(const QString& newLesson) {
+        lessons.push_back(newLesson);
+        saveLessons();
+    }
+
+    void deleteLesson(const QString& delName) {
+        lessons.erase(std::remove_if(lesson.begin(), lessons.end(), [&delName](const QString& item){ return item == delName;}));
+        QFile::remove(delName + ".txt");
+        saveLessons();
+    }
+
+    // words interface =====================================
+    const QVector<QString>& getWords() const { return words; }
+
+    int getWordsSize() const { return words.size(); }
+
+    void addWord(const WordPair& newWP, const QString& lessonName) {
+        words.push_back(newWP);
+        saveWords(lessonName);
+    }
+
+    void deleteWord(const QString& delName, const Qstring& lessonName) {
+        lessons.erase(std::remove_if(words.begin(), words.end(), [&delName](const WordPair& item){ return item.word == delName;}));
+        saveWords(lessonName);
+    }
+
+    // training words interface =====================================
+    void setUpTrainingWords() {
+        trainingWords = words;
+    }
+
+    const QVector<WordPair>& getTrainingWords() const { return trainingWords; }
+
+    int getTrainingSize() const { return trainingWords.size(); }
+
+    void removeTrainingWord(int idx) {
+        trainingWords.erase(trainingWords.begin() + idx);
+    }
+
+
+
+
+
 
 };
 
